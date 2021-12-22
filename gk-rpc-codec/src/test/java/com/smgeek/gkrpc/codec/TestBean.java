0edc49e7b7eda030873f83fd8dc8b158bdb257e2
starkooo <starkooo23@gmail.com>
2021-12-22 18:15:58 +0800

    no message

---
+++ gk-rpc-codec/src/test/java/com/smgeek/gkrpc/codec/TestBean.java
@@ -0,0 +1,10 @@
+package com.smgeek.gkrpc.codec;
+
+import lombok.Data;
+
+@Data
+public class TestBean {
+
+    private String name;
+    private int age;
+}
---
+++ gk-rpc-proto/src/main/java/com/smgeek/gkrpc/ServiceDescriptor.java
@@ -0,0 +1,18 @@
+package com.smgeek.gkrpc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 表示服务
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceDescriptor {
+    private String clazz;
+    private String method;
+    private String returnType;
+    private String[] parameterTypes;
+}
